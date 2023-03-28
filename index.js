6b4579be09d942650fb62cef58a27c85e0ff2b0f
Metranohm <andrewwink@gmail.com>
2023-03-28 14:02:11 -0500

    add number randomizer

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/rand', (req, res) => {
+  res.render('random');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
 })
 
 app.listen(3001, () => {

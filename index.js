2eb9c966d4a8b59f5b21baa271363cfa99b55910
Metranohm <andrewwink@gmail.com>
2023-03-28 15:03:08 -0500

    cat template

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ app.get('/', (req, res) => {
   res.render('home');
 })
 
+app.get('/r/:subreddit', (req, res) => {
+  const { subreddit } = req.params;
+  res.render('subreddit', { subreddit });
+})
+
 app.get('/rand', (req, res) => {
-  res.render('random');
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render('random', { num });
 })
 
 app.get('/about', (req, res) => {
@@ -19,6 +25,10 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+})
+app.get('/cats', (req, res) => {
+  const cats = ['Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'];
+  res.render('cats', { cats });
 })
 
 app.listen(3001, () => {

9f16bf04e3aa52579a810e22d00975ec72f917b2
Metranohm <andrewwink@gmail.com>
2023-03-28 15:27:25 -0500

    create json data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const redditData = require('./data.json');
+console.log(redditData);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
